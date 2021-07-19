ea2fa90737d3c39163df2593655a70f25e447393
SMachen <bembrin@gmail.com>
2021-07-19 18:18:21 +0100

    removing gps threading

--- dashcam.py
+++ dashcam.py
@@ -34,11 +34,10 @@ def get_gps(port, queue):
     This function reads a nmea string from port and parses gps data
     '''
     while True:
-       
         try:
             msg = pynmea2.parse(port.readline())
             if msg.sentence_type == 'RMC':
-                queue.put(msg)
+                return msg #queue.put(msg)
         except:
             pass
 
@@ -48,10 +47,6 @@ def shutdown(cam, vid_file):
     ~Save stream loop
     ~Shutdown pi
     '''
-    # Save current stream to file
-    # filename = format_filename(**args)
-    # print('Saving file to {}...'.format(filename))
-    # stream.copy_to(filename)
     
     # stop the camera
     print('Stopping recording...')
@@ -62,15 +57,17 @@ def shutdown(cam, vid_file):
     print('Stopping system. Goodby...')
     os.system('sudo shutdown -P now')
 
-def highlight(stream, **args):
+def highlight(filename):#stream, **args):
     '''
     Save current loop to perminant folder
     '''
+    perm_file = filename.split('/')[-1]
+    os.system('cp {} ./permenant/{}'.format(filename, perm_file))
     print('Hilighting current loop...')
-    pwd = os.getcwd()
-    parent = os.path.dirname(pwd)
-    save_path = os.path.join(parent,'permanent',format_filename(**args))
-    stream.copy_to(save_path)
+    # pwd = os.getcwd()
+    # parent = os.path.dirname(pwd)
+    # save_path = os.path.join(parent,'permanent',format_filename(**args))
+    # stream.copy_to(save_path)
 
 def format_filename(date=None, time=None, fmrt='%Y-%m-%d%H:%M:%S', **args):
     '''
@@ -81,37 +78,39 @@ def format_filename(date=None, time=None, fmrt='%Y-%m-%d%H:%M:%S', **args):
     filename = now.strftime("%y%m%d%H%M_%d%b%y-%H%M") + '.h264'
     return filename
 
-def gps_loop(q,*args):
-    while True:
-        q.put(get_gps(*args))
-
 def overlay_text(fields):
      overlay_text = '{date} {time} | Posit:ion: {lat} {lat_dir} -- {lon} {lon_dir} | Speed: {speed} {speed_units} | Trk: {trk}'.format(
             **fields
             )
      return overlay_text
+
+
+
 # ================== MAIN FUNCTION =======================================
 
-def main(height=None, width=None, frames=None, clip_dur=None, min_space=None, vid_dir=None,
-         shutdown_pin=None, highlight_pin=None, speed_conversion=1, speed_units=''):
-    # ensure device perameters type
-    res = (int(width), int(height))  # resolution of the video recording 
-    frames = int(frames)   # framerate of the video recording
-    clip_dur = int(clip_dur)    # duration of individual clips
-    min_space = int(min_space)  # the maximum percentage of used storage allowed
-    convert = float(speed_conversion)     # Speed conversion factor
-    # vid_dir = '/home/pi/dashcam/vids'  # Directory where clips are stored
-      
-    # change to the videos directory
-    os.chdir(vid_dir)
+def main(height=None, width=None, frames=None, quality=None, clip_dur=None, min_space=None,
+        vid_dir=None, shutdown_pin=None, highlight_pin=None, speed_conversion=1, speed_units=''):
     
-    # initialize the serial port
+    # ensure device perameters type
+    res = (int(width), int(height))     # resolution of the video recording 
+    frames = int(frames)                # framerate of the video recording
+    quality = int(quality)
+    clip_dur = int(clip_dur)            # duration of individual clips
+    min_space = int(min_space)          # the maximum percentage of used storage allowed
+    convert = float(speed_conversion)   # Speed conversion factor
+    
+    # make video directory if it doesn't exist
+    if not os.isdir(vid_dir):
+        os.system('mkdir {}'.format(vid_dir)
+
+    # initialize gps
     port = Serial('/dev/serial0')
-    gps_q = Queue()
-    gps_TH = Thread(target=get_gps, args=(port, gps_q))
-    gps_TH.daemon = True
-    gps_TH.start()
-    msg = gps_q.get()
+    msg = get_gps(port, None)
+    # gps_q = Queue()
+    # gps_TH = Thread(target=get_gps, args=(port, gps_q))
+    # gps_TH.daemon = True
+    # gps_TH.start()
+    # msg = gps_q.get()
 
     # Initialize the overaly text
     overlay = {
@@ -127,18 +126,26 @@ def main(height=None, width=None, frames=None, clip_dur=None, min_space=None, vi
             }
     
     # Start Recording
-    filename = os.path.join('vids', format_filename(**overlay))
+    filename = os.path.join(vid_dir, format_filename(**overlay))
     vid_file = [open(filename, 'wb')]
     cam = pc.PiCamera(resolution=res, framerate=frames)
     cam.start_recording(vid_file[0], 
                         format='h264',
-                        quality=15,
+                        quality=quality,
                         resize=res,
                         sps_timing=True
                         )
     cam.annotate_background = Color('black')
     cam.annotate_text = overlay_text(overlay)
     print('Recording to {}...'.format(filename)) 
+    
+    # Start Highlight stream
+    # highlight_stream = pc.PiCameraCircularIO(cam, seconds=60, splitter_port=2)
+    # cam.start_recording(highlight_stream, 
+    #                     splitter_port=2,
+    #                     format='h264',
+    #                     quality=30,
+    #                     )
 
     # initialize shutdown button
     shutdown_pin = Button(shutdown_pin, pull_up=False)
@@ -149,8 +156,9 @@ def main(height=None, width=None, frames=None, clip_dur=None, min_space=None, vi
     # main while loop
     while True:
         # check the file system and remove oldest video if not enough storage
+        # file_sweeper(path=('./vids', max_space=min_space)
         file_sweeper_TH = Thread(target=file_sweeper,
-                                kwargs=dict(path=os.getcwd(), max_space=min_space)
+                                kwargs=dict(path='./vids/', max_space=min_space)
                                 )
         file_sweeper_TH.start()
 
@@ -160,7 +168,7 @@ def main(height=None, width=None, frames=None, clip_dur=None, min_space=None, vi
         while time.time() < timeout:
            
             # update overlay text
-            msg = gps_q.get()
+            msg = get_gps(port, None) # gps_q.get()
             overlay['lat'] =msg.lat
             overlay['lat_dir'] = msg.lat_dir, 
             overlay['lon'] = msg.lon 
@@ -180,16 +188,16 @@ def main(height=None, width=None, frames=None, clip_dur=None, min_space=None, vi
             
             # Check highlight button press
             if False:
-                highlight(stream, **overlay)
-            print('Time between shutdown checks: {}'.format(toc))
+                highlight(highlihgt_stream, **overlay)
+            print('Time between shutdown checks: {}\n'.format(toc))
 
 
         # save the current video
-        filename = os.path.join('vids',format_filename(**overlay))
+        filename = os.path.join(vid_dir, format_filename(**overlay))
         vid_file.append(open(filename, 'wb'))
         cam.split_recording(vid_file[-1])
         vid_file.pop(0).close()
-        print('recording to ' + filename )
+        print('\n\n\nrecording to ' + filename + '\n\n')
     
 if __name__ == "__main__":
     # get arguments for main() from the config file
